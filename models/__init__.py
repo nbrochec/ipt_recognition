9ad6a53bf2bef13c09ce6102f04eae4a3c1c3c7d
Nicolas Brochec <47108281+nbrochec@users.noreply.github.com>
2026-04-17 14:52:42 +0200

    Fix import statement for model_utils module

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,6 @@
 from .models import eguitar, flute, base
 from .layers import customConv2d, LogMelSpectrogramLayer
-from .model_utils import LoadModel, ModelSummary, ModelTester, ModelInit, ModelTrainer, PrepareModel, TestSavedModel, ModelChecker, DeviceName, ModelSaver
+from .models_utils import LoadModel, ModelSummary, ModelTester, ModelInit, ModelTrainer, PrepareModel, TestSavedModel, ModelChecker, DeviceName, ModelSaver
 
 __all__=[
     'eguitar', 'flute', 'base',
